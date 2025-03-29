aa3c72ef8663edd37c8fb9ef6f808c67ac021abf
PetrasekMartin <petrasek.martin@seznam.cz>
2025-03-29 20:32:17 +0100

    Fix screen logic

---
+++ Assets/Scripts/UI/SceneLoaderStoryScreen.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderStoryScreen : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("StoryScreen"); // Nahraď názvem tvé scény
+    }
+}
