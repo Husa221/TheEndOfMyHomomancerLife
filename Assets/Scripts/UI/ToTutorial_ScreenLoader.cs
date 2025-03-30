a86a7f1ffbb93c1a4f02aa1e48ba625e25ed225f
PetrasekMartin <petrasek.martin@seznam.cz>
2025-03-30 13:49:17 +0200

    Menu fix

---
+++ Assets/Scripts/UI/ToTutorial_ScreenLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToTutorial_ScreenLoader: MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene("TutorialScreen"); // Nahraď názvem tvé scény
+    }
+}
